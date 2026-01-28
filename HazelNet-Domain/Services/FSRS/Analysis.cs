ee175f2aa19bf1d5bd5af4fe122b5067025fb440
Kaizu-r <jljoloc@gmail.com>
2026-01-29 01:10:24 +0800

    added basic statistical analysis

---
+++ HazelNet-Domain/Services/FSRS/Analysis.cs
@@ -0,0 +1,24 @@
+using HazelNet_Domain.Models;
+
+namespace HazelNet_Domain.Services.FSRS;
+
+public class Analysis
+{
+    public double RecallAccuracy(Card card)
+    {
+        return (card.Reps - card.Lapses) / (double)card.Lapses;
+    }
+
+    public Rating AvgDifficulty(ReviewHistory reviewHistory)
+    {
+        float totalDifficulty = 0;
+        for(int i = 0; i < reviewHistory.ReviewLogs.Count; i++)
+        {
+            //calculate average difficulty from review logs
+            totalDifficulty += (float)reviewHistory.ReviewLogs.ElementAt(i).Rating;
+        }
+        float avgDifficulty = totalDifficulty / reviewHistory.ReviewLogs.Count;
+        avgDifficulty = float.Clamp(avgDifficulty, 2, 4);   //clamp between Hard and Easy
+        return (Rating)Math.Round(avgDifficulty);
+    }
+}

43dd75d4b0c69172bd228f4e8f6ef52a301753cc
Kaizu-r <jljoloc@gmail.com>
2026-01-29 00:45:20 +0800

    Added Card class

---
+++ HazelNet-Domain/Models/Card.cs
@@ -0,0 +1,36 @@
+namespace HazelNet_Domain.Models;
+
+public class Card
+{
+    
+    public int Id { get; private set; }
+    
+    //FSRS PROPERTIES
+    public DateTime Due { get; set; }
+    public double Stability { get; set; }
+    public double Difficulty { get; set; }
+    public ulong ElapsedDays { get; set; }
+    public ulong ScheduledDays { get; set; }
+    public ulong Reps { get; set; }
+    public ulong Lapses { get; set; }
+    public State State { get; set; }
+    public DateTime LastReview { get; set; }
+
+    public Card()
+    {
+        Due = DateTime.MinValue;
+        Stability = 0;
+        Difficulty = 0;
+        ElapsedDays = 0;
+        ScheduledDays = 0;
+        Reps = 0;
+        Lapses = 0;
+        State = State.New;
+        LastReview = DateTime.MinValue;
+    }
+
+    public Card Clone()
+    {
+        return (Card)MemberwiseClone();
+    }
+}

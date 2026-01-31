d3a9bd6096015670e667b27bc9e1780fb3243c3a
Vhon Michael <vhontabalan@gmail.com>
2026-02-01 03:28:25 +0800

    feat: specified deck and relation config for database

---
+++ HazelNet-Infrastracture/DBContext/DeckEntityTypeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using HazelNet_Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HazelNet_Infrastracture.DBContext;
+
+public class DeckEntityTypeConfiguration :IEntityTypeConfiguration<Deck>
+{
+    public void Configure(EntityTypeBuilder<Deck> builder)
+    {
+        builder.HasKey(d => d.Id);
+        builder.Property(d => d.DeckName)
+            .HasMaxLength(100)
+            .IsRequired();
+        
+        builder.HasMany(d => d.Cards)
+            .WithOne(c => c.Deck)
+            .HasForeignKey(d => d.DeckId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}

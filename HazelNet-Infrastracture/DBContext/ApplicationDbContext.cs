6e85b3f06534699263377ed86438061e3ba7c48e
Vhon Michael <vhontabalan@gmail.com>
2026-02-01 03:29:15 +0800

    feat: created and specified the db context

---
+++ HazelNet-Infrastracture/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using HazelNet_Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace HazelNet_Infrastracture.DBContext;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Deck> Decks { get; set; }
+    public DbSet<Card> Cards { get; set; }
+    
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        :base(options) {}
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.ApplyConfiguration(new CardsEntityTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new DeckEntityTypeConfiguration());
+    }
+}
+

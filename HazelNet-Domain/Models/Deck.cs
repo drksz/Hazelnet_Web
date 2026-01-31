d4fad7cedb7e3461b5ad2e2d70fb165241b80b35
Vhon Michael <vhontabalan@gmail.com>
2026-02-01 03:26:53 +0800

    feat: Created deck for grouping of cards

---
+++ HazelNet-Domain/Models/Deck.cs
@@ -0,0 +1,11 @@
+﻿namespace HazelNet_Domain.Models;
+
+public class Deck
+{
+    public int Id { get; set; }
+    public string DeckName { get; set; }
+    public DateTime CreationDate { get; set; } = DateTime.Now;
+    public DateTime LastAcess { get; set; } = DateTime.Now;
+    
+    public ICollection<Card> Cards { get; set; } = new List<Card>();
+}

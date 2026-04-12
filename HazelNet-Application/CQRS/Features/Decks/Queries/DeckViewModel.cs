ef8421c33a07ecf77b3e34efb23286a40665dc0d
Vhon Michael <vhontabalan@gmail.com>
2026-04-13 03:32:22 +0800

    refactor: move DeackViewModel to application and calculate master card percent

---
+++ HazelNet-Application/CQRS/Features/Decks/Queries/DeckViewModel.cs
@@ -0,0 +1,23 @@
+﻿using HazelNet_Domain.Models;
+
+namespace HazelNet_Web.ViewModel;
+
+public class DeckViewModel
+{
+    public string Name;
+    public string? Description;
+    public int TotalNumberOfCards = 0;
+    public DateTime LastDateAccessed;
+        
+    //I need a function for the following 
+    public int MasteredCards { get; set; }
+    
+    public double MasteredPercentage =>
+        TotalNumberOfCards == 0 ? 0 :
+            (double)MasteredCards / TotalNumberOfCards * 100;
+
+    public int NumOfDueCards { get; set; } = 0;
+
+    
+  
+}

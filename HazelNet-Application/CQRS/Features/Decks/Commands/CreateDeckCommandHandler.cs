be87ae083ae27d181fca8788292cabdc207e581e
Vhon Michael <vhontabalan@gmail.com>
2026-04-21 04:39:28 +0800

    feat: inject and current  user userId in creating new deck

--- HazelNet-Application/CQRS/Features/Decks/Commands/CreateDeckCommandHandler.cs
+++ HazelNet-Application/CQRS/Features/Decks/Commands/CreateDeckCommandHandler.cs
@@ -1,4 +1,6 @@
 ﻿using HazelNet_Application.CQRS.Abstractions;
+using HazelNet_Application.CQRS.Abstractions.Identity;
+using HazelNet_Application.Interface;
 using HazelNet_Domain.IRepository;
 using HazelNet_Domain.Models;
 
@@ -6,23 +8,32 @@ namespace HazelNet_Application.CQRS.Features.Decks.Commands;
 
 public class CreateDeckCommandHandler : ICommandHandler<CreateDeckCommand, int>
 {
-    private IDeckRepository _deckRepository;
+    private readonly ICurrentUserService _currentUserService;
+    private readonly IDeckRepository _deckRepository;
 
-    public CreateDeckCommandHandler(IDeckRepository deckRepository)
+    public CreateDeckCommandHandler(IDeckRepository deckRepository,  ICurrentUserService currentUserService)
     {
         _deckRepository = deckRepository;
+        _currentUserService = currentUserService;
     }
 
 
     public async Task<int> Handle(CreateDeckCommand command)
     {
+        var userIdString = await _currentUserService.GetUserIdAsync();
+        
+        if (!int.TryParse(userIdString, out var userId))
+        {
+            throw new Exception("User is not authenticated or token is invalid.");
+        }
+        
         var deck = new Deck
         {
             DeckName = command.DeckName,
             DeckDescription = command.DeckDescription,
             CreationDate = DateTime.UtcNow,
             LastAcess = DateTime.UtcNow,
-            UserId = command.UserId,
+            UserId = userId
         };
         
         await _deckRepository.CreateAsync(deck);

34b17e92d54afab46c3d12db3ba4d7d8adcbb43e
Vhon Michael <vhontabalan@gmail.com>
2026-04-21 04:34:41 +0800

    feat: create UpdateDeckCommand and Handler

---
+++ HazelNet-Application/CQRS/Features/Decks/Commands/UpdateDeckCommand.cs
@@ -0,0 +1,6 @@
+﻿using HazelNet_Application.CQRS.Abstractions;
+
+namespace HazelNet_Application.CQRS.Features.Decks.Commands;
+
+public record UpdateDeckCommand(int DeckId, string DeckName, string DeckDescription)
+    : ICommand;

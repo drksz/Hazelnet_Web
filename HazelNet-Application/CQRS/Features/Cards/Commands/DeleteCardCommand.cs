9b36fd36c12e8261e37a87b2be2dc67c9d6973c7
Vhon Michael <vhontabalan@gmail.com>
2026-04-29 00:31:45 +0800

    feat: create command that deletes cards using card id with user validation

---
+++ HazelNet-Application/CQRS/Features/Cards/Commands/DeleteCardCommand.cs
@@ -0,0 +1,8 @@
+﻿using HazelNet_Application.CQRS.Abstractions;
+
+namespace HazelNet_Application.CQRS.Features.Cards.Commands;
+
+public record DeleteCardCommand
+    (
+        int CardId
+    ): ICommand;

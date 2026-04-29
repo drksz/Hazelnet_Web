310f2f8ad9fe1877bc6184e6154d4d6e37bb1eb3
Kaizu-r <jljoloc@gmail.com>
2026-04-29 21:18:35 +0800

    feature: added FSRSParameters Repo interface

--- HazelNet-Domain/IRepository/IFSRSParametersRepository.cs
+++ HazelNet-Domain/IRepository/IFSRSParametersRepository.cs
@@ -2,3 +2,11 @@ using HazelNet_Domain.Models;
 
 
 namespace HazelNet_Domain.IRepository;
+
+public interface IFSRSParametersRepository
+{
+    Task<FSRSParameters?> GetFSRSParametersByIdAsync(int id);
+    Task<FSRSParameters?> GetFSRSParametersByUserIdAsync(int userId);
+    Task UpdateAsync(FSRSParameters parameters);
+    Task CreateAsync(FSRSParameters parameters);
+}

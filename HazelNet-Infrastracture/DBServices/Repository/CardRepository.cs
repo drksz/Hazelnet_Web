c6f43c5ca6d39b2ad8e6746379bb797b3f342512
Vhon Michael <vhontabalan@gmail.com>
2026-05-05 01:28:52 +0800

    feat:fix: resolve concurrent DbContext issue by changing to DBContextFactory

--- HazelNet-Infrastracture/DBServices/Repository/CardRepository.cs
+++ HazelNet-Infrastracture/DBServices/Repository/CardRepository.cs
@@ -10,20 +10,24 @@ namespace HazelNet_Infrastracture.DBServices.Repository;
 //implementation of icardrepository 
 public class CardRepository : ICardRepository
 {
-    private readonly ApplicationDbContext _context;
+    private readonly IDbContextFactory<ApplicationDbContext> _contextFactory;
 
-    public CardRepository(ApplicationDbContext context)
+    public CardRepository( IDbContextFactory<ApplicationDbContext> context)
     {
-        _context = context;
+        _contextFactory = context;
     }
     
     public async Task<Card?> GetCardByIdAsync(int cardId)
     {
-        return await _context.Cards.FindAsync(cardId);
+        await using var _context = await _contextFactory.CreateDbContextAsync();
+        return await _context.Cards
+            .Include(c => c.ReviewHistory)
+            .FirstOrDefaultAsync(c => c.Id == cardId);
     }
 
     public async Task<List<Card>> GetAllCardByDeckIdAsync(int deckId)
     {
+        await using var _context = await _contextFactory.CreateDbContextAsync();
         return await _context.Cards
             .Where(c => c.DeckId == deckId)
             .ToListAsync();
@@ -31,12 +35,14 @@ public class CardRepository : ICardRepository
 
     public async Task UpdateAsync(Card card)
     {
+        await using var _context = await _contextFactory.CreateDbContextAsync();
         _context.Cards.Update(card);
         await _context.SaveChangesAsync();
     }
 
     public async Task DeleteAsync(int cardId)
     {
+        await using var _context = await _contextFactory.CreateDbContextAsync();
         var card = await GetCardByIdAsync(cardId);
         if (card != null)
         {
@@ -47,6 +53,7 @@ public class CardRepository : ICardRepository
 
     public async Task CreateAsync(Card card)
     {
+        await using var _context = await _contextFactory.CreateDbContextAsync();
         await _context.Cards.AddAsync(card);
         await _context.SaveChangesAsync();
     }
--- HazelNet-Infrastracture/DBServices/Repository/DeckRepository.cs
+++ HazelNet-Infrastracture/DBServices/Repository/DeckRepository.cs
@@ -10,22 +10,26 @@ namespace HazelNet_Infrastracture.DBServices.Repository;
 //implementation of ideckrepository
 public class DeckRepository : IDeckRepository
 {
-    private readonly ApplicationDbContext _context;
+    private readonly IDbContextFactory<ApplicationDbContext> _contextFactory;
 
-    public DeckRepository(ApplicationDbContext context)
+    public DeckRepository(IDbContextFactory<ApplicationDbContext> context)
     {
-        _context = context;
+        _contextFactory = context;
     }
 
     public async Task<Deck?> GetDeckByIdAsync(int deckId)
     {
-        return await _context.Decks.FirstOrDefaultAsync(d => d.Id == deckId);
+        await using var _context = await _contextFactory.CreateDbContextAsync();
+        return await _context.Decks
+            .Include(d => d.Cards)
+            .FirstOrDefaultAsync(d => d.Id == deckId);
     }
     
 
 
     public async Task<List<Deck>> GetAllDeckByUserIdAsync(int userId)
     {
+        await using var _context = await _contextFactory.CreateDbContextAsync();
         return await _context.Decks
             .Where(d => d.UserId == userId)
             .ToListAsync();
@@ -33,24 +37,28 @@ public class DeckRepository : IDeckRepository
 
     public async Task UpdateDeckAsync(Deck deck)
     {
+        await using var _context = await _contextFactory.CreateDbContextAsync();
         _context.Decks.Update(deck);
         await _context.SaveChangesAsync();
     }
 
     public async Task DeleteDeckAsync(Deck _deck)
     {
+        await using var _context = await _contextFactory.CreateDbContextAsync();
        _context.Decks.Remove(_deck);
        await _context.SaveChangesAsync();
     }
 
     public async Task CreateAsync(Deck deck)
     {
+        await using var _context = await _contextFactory.CreateDbContextAsync();
          _context.Decks.Add(deck);
         await _context.SaveChangesAsync();
     }
 
     public async Task ClearAllCardsInDeckAsync(int deckId)
     {
+        await using var _context = await _contextFactory.CreateDbContextAsync();
         var cards = await _context.Cards.Where(c => c.DeckId == deckId).ExecuteDeleteAsync();
     }
 }
